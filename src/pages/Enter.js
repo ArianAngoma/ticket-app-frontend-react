974849d06fdc22419cfe4a103548c946fb925819
unknown <arian.angoma@tecsup.edu.pe>
2021-10-28 01:34:48 -0500

    Formulario para ingresar

--- src/pages/Enter.js
+++ src/pages/Enter.js
@@ -1,7 +1,69 @@
+import {Button, Form, Input, InputNumber} from 'antd';
+import {SaveOutlined} from '@ant-design/icons';
+
 export const Enter = () => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>
-            Enter Page
-        </div>
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                span: 14,
+            }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="Nombre del agente"
+                name="agent"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Por favor ingrese su nombre',
+                    },
+                ]}
+            >
+                <Input/>
+            </Form.Item>
+
+            <Form.Item
+                label="Escritorio"
+                name="desk"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Por favor ingrese el número de escritorio',
+                    },
+                ]}
+            >
+                <InputNumber min={1} max={99}/>
+            </Form.Item>
+
+            <Form.Item
+                wrapperCol={{
+                    offset: 8,
+                    span: 14,
+                }}
+            >
+                <Button type="primary" htmlType="submit"
+                        shape="round">
+                    <SaveOutlined/>
+                    Ingresar
+                </Button>
+            </Form.Item>
+        </Form>
     )
 }
