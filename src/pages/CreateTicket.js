407b488c4f9804280c3ade9dbca6812fc63675cb
unknown <arian.angoma@tecsup.edu.pe>
2021-10-31 05:10:03 -0500

    Crear nuevo ticket

--- src/pages/CreateTicket.js
+++ src/pages/CreateTicket.js
@@ -1,8 +1,10 @@
 import {Button, Col, Row, Typography} from 'antd';
 import {DownloadOutlined} from '@ant-design/icons';
+import {useContext, useState} from 'react';
 
 /* Importaciones propias */
 // import {useUiMenu} from '../hooks/useUiMenu';
+import {SocketContext} from '../context/SocketContext';
 
 const {Title, Text} = Typography;
 
@@ -10,8 +12,21 @@ export const CreateTicket = () => {
     /* Hook para el menú */
     // useUiMenu(true);
 
+    const {socket} = useContext(SocketContext);
+
+    /* Estado del ticket */
+    const [ticket, setTicket] = useState(null);
+
+    /* Emitir evento para solicitar nuevo ticket */
     const newTicket = () => {
-        console.log('New Tickewt')
+
+        /* El segundo argumento es la data, que puede ir o no; en este caso no se envía ni una data
+        *  El tercer argumento es un callback si el backend quiere, lo ejecuta; en este caso se recibe ticket creado por el backend
+        *  */
+        socket.emit('req-ticket', null, (ticket) => {
+            // console.log(ticket);
+            setTicket(ticket);
+        });
     }
 
     return (
@@ -31,13 +46,17 @@ export const CreateTicket = () => {
                 </Col>
             </Row>
 
-            <Row style={{marginTop: 100}}>
-                <Col span={14} offset={6} align="center">
-                    <Text lavel={2}>Su número</Text>
-                    <br/>
-                    <Text type="success" style={{fontSize: 55}}>55</Text>
-                </Col>
-            </Row>
+            {
+                ticket && (
+                    <Row style={{marginTop: 100}}>
+                        <Col span={14} offset={6} align="center">
+                            <Text lavel={2}>Su número</Text>
+                            <br/>
+                            <Text type="success" style={{fontSize: 55}}>{ticket.number}</Text>
+                        </Col>
+                    </Row>
+                )
+            }
         </>
     )
 }
